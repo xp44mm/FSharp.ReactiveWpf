5ebd244d6bae56070643f72a9e911a7d9c9251c7
xp44mm <34696643@qq.com>
2026-02-01 17:11:21 +0800

    20260201

---
+++ FSharp.ReactiveWpf.Test/Tests.fs
@@ -0,0 +1,38 @@
+﻿module Tests
+
+open System
+open Xunit
+
+open System
+open System.Reactive.Linq
+open FSharp.ReactiveWpf
+
+// 创建并使用ObservableArray
+let demo() =
+    use array = new ObservableArray<string>()
+        
+    // 订阅具体的变更事件
+    let subscription2 =
+        array.changes
+            .Subscribe(function
+                | CollectionChange.Added(i, item) ->
+                    printfn "添加了项目 [%d]: %s" i item
+                | CollectionChange.Removed(i, item) ->
+                    printfn "移除了项目 [%d]: %s" i item
+                | CollectionChange.Replaced(i, oldItem, newItem) ->
+                    printfn "替换了项目 [%d]: %s -> %s" i oldItem newItem
+                | CollectionChange.Cleared ->
+                    printfn "清空了数组"
+                | _ -> ())
+        
+    // 执行操作
+    array.Add("第一个项目")
+    array.Add("第二个项目")
+    array.Insert(1, "中间项目")
+    array.[0] <- "修改后的第一个项目"
+    array.RemoveAt(1)
+    array.Clear()
+    
+
+// 运行示例
+demo()
---
+++ FSharp.ReactiveWpf/ObservableArray.fs
@@ -0,0 +1,68 @@
+﻿namespace FSharp.ReactiveWpf
+
+open System
+open System.Collections
+open System.Collections.Generic
+open System.Reactive.Linq
+open System.Reactive.Subjects
+
+[<RequireQualifiedAccess>]
+type CollectionChange<'T> =
+    | Added of index: int * item: 'T
+    | Removed of index: int * item: 'T
+    | Replaced of index: int * oldItem: 'T * newItem: 'T
+    | Cleared
+
+type ObservableArray<'T>() =
+    let items = ResizeArray<'T>()
+    let subject =
+        new BehaviorSubject<IReadOnlyList<'T>>(items :> IReadOnlyList<'T>)
+    let changesSubject = new Subject<CollectionChange<'T>>()
+
+    member _.latestValue = subject.AsObservable()
+    member _.changes = changesSubject.AsObservable()
+
+    member _.Add(item: 'T) =
+        let index = items.Count
+        items.Add(item)
+        changesSubject.OnNext(CollectionChange.Added(index, item))
+        subject.OnNext(items :> IReadOnlyList<'T>)
+
+    member _.Insert(index: int, item: 'T) =
+        items.Insert(index, item)
+        changesSubject.OnNext(CollectionChange.Added(index, item))
+        subject.OnNext(items :> IReadOnlyList<'T>)
+
+    member _.Remove(item: 'T) =
+        let index = items.IndexOf(item)
+        if index >= 0 then
+            items.RemoveAt(index)
+            changesSubject.OnNext(CollectionChange.Removed(index, item))
+            subject.OnNext(items :> IReadOnlyList<'T>)
+
+    member _.RemoveAt(index: int) =
+        let item = items.[index]
+        items.RemoveAt(index)
+        changesSubject.OnNext(CollectionChange.Removed(index, item))
+        subject.OnNext(items :> IReadOnlyList<'T>)
+
+    member _.Clear() =
+        if items.Count > 0 then
+            items.Clear()
+            changesSubject.OnNext(CollectionChange.Cleared)
+            subject.OnNext(items :> IReadOnlyList<'T>)
+
+    member _.Item
+        with get (index) = items.[index]
+        and set index value =
+            let oldItem = items.[index]
+            items.[index] <- value
+            changesSubject.OnNext(CollectionChange.Replaced(index, oldItem, value))
+            subject.OnNext(items :> IReadOnlyList<'T>)
+
+    interface IDisposable with
+        member _.Dispose() =
+            changesSubject.OnCompleted()
+            subject.OnCompleted()
+            changesSubject.Dispose()
+            subject.Dispose()
