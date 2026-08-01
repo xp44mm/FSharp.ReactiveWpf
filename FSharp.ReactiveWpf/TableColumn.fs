3eaf0f4d6ca26da5a85d373315521a9aa141e485
xp44mm <34696643@qq.com>
2026-08-01 18:54:45 +0800

    appendTo

--- FSharp.ReactiveWpf/TableColumn.fs
+++ FSharp.ReactiveWpf/TableColumn.fs
@@ -4,27 +4,29 @@ open System.Windows
 open System.Windows.Documents
 open System.Windows.Media
 
-/// 设置列宽
+/// col.Width <- value
 let width (value: GridLength) (col: TableColumn) =
     col.Width <- value
     col
 
-/// 设置列宽（像素值）
+/// col.Width <- GridLength(value)
 let widthPixels (value: double) (col: TableColumn) =
     col.Width <- GridLength(value)
     col
 
-/// 设置列宽（星号比例）
+/// col.Width <- GridLength(value, GridUnitType.Star)
 let widthStar (value: double) (col: TableColumn) =
     col.Width <- GridLength(value, GridUnitType.Star)
     col
 
-/// 设置列宽（自动）
+/// col.Width <- GridLength.Auto
 let widthAuto (col: TableColumn) =
     col.Width <- GridLength.Auto
     col
 
-/// 设置列背景
+/// col.Background <- brush
 let background (brush: Brush) (col: TableColumn) =
     col.Background <- brush
     col
+/// tbl.Columns.Add(col)
+let appendTo (tbl: Table) (col: TableColumn) = tbl.Columns.Add(col)
